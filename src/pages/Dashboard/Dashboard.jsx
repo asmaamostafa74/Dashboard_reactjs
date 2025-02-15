f358d44a568aa0e5348662909226f515f5d10946
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-15 13:49:15 +0200

    Remove step 3

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import Numbers from "../../components/Dashboard/Numbers"
 import Step2 from "../../components/Dashboard/Step2"
-import Step3 from "../../components/Dashboard/step3"
 import Step4 from "../../components/Dashboard/Step4"
 
 const Dashboard = () => {
@@ -8,7 +7,6 @@ const Dashboard = () => {
         <div className="container mx-auto">
             <Numbers />
             <Step2 />
-            <Step3 />
             <Step4 />
         </div>
     )

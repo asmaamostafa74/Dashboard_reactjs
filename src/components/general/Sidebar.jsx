65007cb428fcfe723feb62cd1d99f8ccf667667f
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-16 18:37:11 +0200

    Add translation

--- src/components/general/Sidebar.jsx
+++ src/components/general/Sidebar.jsx
@@ -5,7 +5,9 @@ import { IoHome, IoStatsChart } from "react-icons/io5"
 import { NavLink } from "react-router"
 import logo from "../../assets/images/logo-creative-tim-black.svg"
 import { FaFile } from "react-icons/fa"
+import { useTranslation } from "react-i18next";
 const Sidebar = () => {
+  const { t, i18n } = useTranslation();
   const [isRTL, setIsRTL] = useState(localStorage.getItem("direction") === "rtl");
 
   useEffect(() => {
@@ -14,28 +16,43 @@ const Sidebar = () => {
   }, [isRTL]);
 
   const toggleDirection = () => {
-    setIsRTL((prev) => !prev);
+    setIsRTL((prev) => {
+      const newRTL = !prev;
+      const newLang = newRTL ? "ar" : "en";
+
+      localStorage.setItem("direction", newRTL ? "rtl" : "ltr");
+      localStorage.setItem("language", newLang);
+
+      if (i18n && typeof i18n.changeLanguage === "function") {
+        i18n.changeLanguage(newLang);
+      } else {
+        console.error("i18n.changeLanguage is not a function");
+      }
+
+      return newRTL;
+    });
   };
+
   return (
     <aside className="sidebar w-[70px] md:w-[246px] h-screen bg-[#F8F9FA]">
       <div className="flex items-center justify-center flex-col md:flex-row gap-3 p-3">
         <img src={logo} alt="Logo" />
-        <h1 className="hidden md:block text-center text-sm md:text-xl font-medium">UI Dashboard</h1>
+        <h1 className="hidden md:block text-center text-sm md:text-xl font-medium">{t('uiDash')}</h1>
       </div>
       <div className="h-px w-44 bg-gray-100 mx-auto"></div>
       <div className="flex flex-col gap-4 px-3 py-3 md:px-5 md:py-5">
         <div>
           <NavLink to='dashboard' className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4">
             <IoHome className="p-2 rounded-full" size={35} />
-            <p className="hidden md:block text-[#A0AEC0]">Dashboad</p>
+            <p className="hidden md:block text-[#A0AEC0]">{t('welcome')}</p>
           </NavLink>
           <NavLink to='/tables' className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4">
             <IoStatsChart className="p-2 rounded-full" size={35} />
-            <p className="hidden md:block text-[#A0AEC0]">Tables</p>
+            <p className="hidden md:block text-[#A0AEC0]">{t('tables')}</p>
           </NavLink>
           <NavLink to='/billing' className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4">
             <BsCreditCardFill className="p-2 rounded-full" size={35} />
-            <p className="hidden md:block text-[#A0AEC0]">Billing</p>
+            <p className="hidden md:block text-[#A0AEC0]">{t('billing')}</p>
           </NavLink>
           <div to="#" className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4 cursor-pointer" onClick={toggleDirection}>
             <IoIosSettings className="p-2 rounded-full text-[#4fd1c5]" size={35} />
@@ -43,20 +60,20 @@ const Sidebar = () => {
           </div>
         </div>
       </div>
-      <h2 className="md:flex items-center justify-center uppercase text-[#2D3748] text-xs font-bold hidden">Account Pages</h2>
+      <h2 className="md:flex items-center justify-center uppercase text-[#2D3748] text-base font-bold hidden">{t("accountPages")}</h2>
       <div className="flex flex-col gap-4 px-3 py-3 md:px-5 md:py-5">
         <div>
           <NavLink to='/profile' className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4">
             <IoStatsChart className="p-2 rounded-full" size={35} />
-            <p className="hidden md:block text-[#A0AEC0]">Profile</p>
+            <p className="hidden md:block text-[#A0AEC0]">{t('profile')}</p>
           </NavLink>
           <NavLink to='/signIn' className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4">
             <FaFile className="p-2 rounded-full" size={35} />
-            <p className="hidden md:block text-[#A0AEC0]">Sign In</p>
+            <p className="hidden md:block text-[#A0AEC0]">{t('signIn')}</p>
           </NavLink>
           <NavLink to='/signUp' className="flex items-center justify-center md:justify-start gap-2 rounded-[15px] h-[54px] md:px-4">
             <IoIosRocket className="p-2 rounded-full" size={35} />
-            <p className="hidden md:block text-[#A0AEC0]">Sign Up</p>
+            <p className="hidden md:block text-[#A0AEC0]">{t('signUp')}</p>
           </NavLink>
         </div>
       </div>

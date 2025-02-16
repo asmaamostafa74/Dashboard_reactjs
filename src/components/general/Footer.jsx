463e636911cf942785b4360c7041ad9d2088acce
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-16 18:51:12 +0200

    Add footer

---
+++ src/components/general/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="container mx-auto mt-5">
+            <p className="text-[#A0AEC0]">@ 2024, Made with ❤️ by
+                <span className="text-[#4fd1c5]"> Creative Tim </span>
+                & <span className="text-[#4fd1c5]"> Simmmple </span>
+                for a better web
+            </p>
+        </footer>
+    )
+}
+
+export default Footer
--- src/pages/DashboradLayout.jsx
+++ src/pages/DashboradLayout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router"
 import Sidebar from "../components/general/Sidebar"
 import PathName from "../components/general/PathName"
+import Footer from "../components/general/Footer"
 
 const DashboradLayout = () => {
   return (
@@ -9,6 +10,7 @@ const DashboradLayout = () => {
       <div className="flex-grow p-4 max-h-[100vh] overflow-auto bg-gray-50">
         <PathName />
         <Outlet />
+        <Footer />
       </div>
     </div>
   )

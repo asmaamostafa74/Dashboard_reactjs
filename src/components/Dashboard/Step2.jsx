b4cbc704d313a51644b9011371a4e250ddf9c965
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2025-02-18 12:06:32 +0200

    return step2 and add responsive

--- src/components/Dashboard/Step2.jsx
+++ src/components/Dashboard/Step2.jsx
@@ -6,7 +6,7 @@ import sImage from '../../assets/images/brooke-cagle.webp'
 const Step2 = () => {
     return (
         <div className="flex flex-wrap gap-2 mt-5">
-            <div className="flex lg:flex-row flex-col items-center justify-between bg-white rounded-[25px] p-5">
+            <div className="flex lg:flex-row flex-col items-center justify-between lg:w-[922px] md:h-[290px] bg-white rounded-[25px] p-5">
                 <div className>
                     <p className="text-[#A0AEC0] text-xs font-bold">Built by developers</p>
                     <h2 className="text-lg text-[#2D3748] font-bold">Purity UI Dashboard</h2>
@@ -20,9 +20,9 @@ const Step2 = () => {
                 </div>
                 <img src={image} alt="Image" />
             </div>
-            {/* <div className="md:w-[600px] md:h-[290.5px] p-5 flex items-center justify-center bg-white rounded-[25px]">
+            <div className="md:w-[600px] md:h-[290.5px] p-5 flex items-center justify-center bg-white rounded-[25px]">
                 <img src={sImage} className="w-[550px]" alt="" />
-            </div> */}
+            </div>
         </div>
     )
 }
